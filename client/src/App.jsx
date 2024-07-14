82029c783eec01df7b896a08e3699bb58b9fcc90
Ostend Suryajaya <osuryaja@uci.edu>
2024-07-14 12:58:30 -0700

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { Text } from "@chakra-ui/react";
+
+import "./App.css";
+
+const App = () => {
+    return (
+        <>
+            <Text>Hello World</Text>
+        </>
+    );
+};
+
+export default App;
