46e2aec781e0d72bb3976c5d2961b23fa1af8f46
Kevin Wu <kevinwu098@gmail.com>
2024-07-14 13:52:06 -0700

    feat: actually add code

---
+++ client/src/App.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import {
+    Box,
+    Table,
+    TableCaption,
+    TableContainer,
+    Tbody,
+    Td,
+    Tfoot,
+    Th,
+    Thead,
+    Tr,
+} from "@chakra-ui/react";
+import axios from "axios";
+
+import IntroMessage from "./components/IntroMessage";
+
+const Backend = axios.create({
+    baseURL: "http://localhost:3001",
+    withCredentials: true,
+});
+
+const App = () => {
+    const getData = async () => {
+        const data = await Backend.get(`/`);
+        console.log(data);
+    };
+
+    getData();
+
+    return (
+        <Box
+            display={"flex"}
+            flexDirection={"column"}
+            maxWidth={1400}
+            marginX={"auto"}
+        >
+            <IntroMessage />
+
+            <TableContainer>
+                <Table variant="simple">
+                    <TableCaption>
+                        Imperial to metric conversion factors
+                    </TableCaption>
+                    <Thead>
+                        <Tr>
+                            <Th>To convert</Th>
+                            <Th>into</Th>
+                            <Th isNumeric>multiply by</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        <Tr>
+                            <Td>inches</Td>
+                            <Td>millimetres (mm)</Td>
+                            <Td isNumeric>25.4</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>feet</Td>
+                            <Td>centimetres (cm)</Td>
+                            <Td isNumeric>30.48</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>yards</Td>
+                            <Td>metres (m)</Td>
+                            <Td isNumeric>0.91444</Td>
+                        </Tr>
+                    </Tbody>
+                    <Tfoot>
+                        <Tr>
+                            <Th>To convert</Th>
+                            <Th>into</Th>
+                            <Th isNumeric>multiply by</Th>
+                        </Tr>
+                    </Tfoot>
+                </Table>
+            </TableContainer>
+        </Box>
+    );
+};
+
+export default App;
---
+++ client/src/components/IntroMessage.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { Box, Button, Link, Text } from "@chakra-ui/react";
+
+const IntroMessage = () => {
+    const [clickedLink, setClickedLink] = useState(false);
+
+    return (
+        <Box
+            marginY={4}
+            gap={4}
+        >
+            <Text
+                fontSize={"xx-large"}
+                fontWeight={"semibold"}
+                color={"purple"}
+            >
+                CTC Practice 24/25
+            </Text>
+
+            <Box
+                display="flex"
+                flexDirection={"column"}
+                gap={2}
+            >
+                <Text as="p">
+                    Your task here, as described in the project document, is to
+                    fetch data from your backend and database, and to then
+                    display it on this page. There's no need to style your work,
+                    as our primary focus is on assessing the core
+                    implementation. Use Chakra for your UI components and Axios
+                    for data fetching.
+                </Text>
+                <Text as="p">
+                    You're also expected to implement alphabetical sorting of
+                    the "Project Name" column. When the header is clicked, the
+                    data should be sorted alphabetically (ascending a-z), and
+                    when clicked again it should revert to it's original order.
+                </Text>
+                <Text
+                    as="p"
+                    display={"inline"}
+                >
+                    Here are some links that may be of use:{" "}
+                    <Link
+                        href="https://www.youtube.com/watch?v=exzQkvv7g1w"
+                        target="_blank"
+                        referrerPolicy="no-referrer"
+                    >
+                        <Button
+                            variant={"link"}
+                            onClick={() => setClickedLink(true)}
+                        >
+                            Chakra Docs&nbsp;
+                            {clickedLink ? (
+                                <Text
+                                    fontWeight={"semibold"}
+                                    fontStyle={"italic"}
+                                    color={"purple"}
+                                >
+                                    (trolled lol)
+                                </Text>
+                            ) : null}
+                        </Button>
+                    </Link>
+                </Text>
+            </Box>
+        </Box>
+    );
+};
+
+export default IntroMessage;
