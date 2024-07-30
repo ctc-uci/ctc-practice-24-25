fcf454c54607116cfa02146dfcc7dd2b797bba12
Sean Kelman <seanrkelman@gmail.com>
2024-07-30 11:53:42 -0700

    added schema for project info and .env

---
+++ server/server/schema/sk_project_info.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS sk_project_info CASCADE;
+
+CREATE SEQUENCE project_id_seq
+INCREMENT 1
+START 1;
+
+CREATE TABLE IF NOT EXISTS sk_project_info
+(
+  id integer NOT NULL DEFAULT nextval('project_id_seq'::regclass) PRIMARY KEY,
+  npo_id integer NOT NULL REFERENCES npo_info (id) ON DELETE CASCADE,
+  start_year integer NOT NULL,
+  end_year integer NOT NULL,
+  project_leads text[3]
+);
