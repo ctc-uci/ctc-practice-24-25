fae491681c774eddd433009c1d73caf54a6eb48d
Nathan Pietrantonio <pietrann@uci.edu>
2024-08-04 16:23:04 -0700

    refactor: change get query
    Instead of returning NPO id as id, return the project specific id

--- server/routes/router.js
+++ server/routes/router.js
@@ -15,7 +15,7 @@ npoRouter.get("/", async (req, res) => {
         // Get all projects in the DB
         const data = await db.query(
             `
-            SELECT npo_info.*, project.npo_id, project.start_year, project.end_year, project.project_leads
+            SELECT npo_info.name, npo_info.description, project.*
             FROM npo_info
             INNER JOIN np_project_info AS project ON npo_info.id=project.npo_id;
             `
