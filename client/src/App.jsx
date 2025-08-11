85d74505898754ff28f636d5c08964934e24c705
brelieu05 <brendanlieu05@gmail.com>
2025-08-11 10:22:21 -0700

    feat: volunteer table

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,8 +12,9 @@ import {
     Tr,
 } from "@chakra-ui/react";
 import axios from "axios";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import IntroMessage from "./components/IntroMessage";
+import { VolunteerTable } from "./volunteerTable";
 
 const Backend = axios.create({
     baseURL: "http://localhost:3001",
@@ -37,7 +38,7 @@ const App = () => {
         fetchData();
     }, []);
     console.log(npoData);
-    return (
+    const NPOContent = () => (
         <Box
             display={"flex"}
             flexDirection={"column"}
@@ -77,6 +78,27 @@ const App = () => {
                 </Table>
             </TableContainer>
         </Box>
+    );
+
+    const VolunteerContent = () => (
+        <Box
+            display={"flex"}
+            flexDirection={"column"}
+            maxWidth={1400}
+            marginX={"auto"}
+        >
+            <IntroMessage />
+            <VolunteerTable />
+        </Box>
+    );
+
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<NPOContent />} />
+                <Route path="/volunteers" element={<VolunteerContent />} />
+            </Routes>
+        </Router>
     );
 };
 
